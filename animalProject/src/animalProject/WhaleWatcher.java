2fe51d951518fa833ac26f49f6aa8c510e9a41f6
Sasha Simms <sashasimms@Sashas-MacBook-Pro.local>
2018-12-12 20:56:41 +0000

    first commit

---
+++ animalProject/src/animalProject/WhaleWatcher.java
@@ -0,0 +1,43 @@
+package animalProject;
+
+import java.util.ArrayList;
+
+public class WhaleWatcher {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Objects
+		Whale rightWhale 	= new Whale("Right Whale", "Atlantic", 2001, 21, 16);
+		Whale blueWhale 	= new Whale("Blue Whale", "Pacific", 2001, 23, 16);
+		Whale spermWhale 	= new Whale("Sperm Whale", "Atlantic", 1900, 20, 40);
+		Whale humpbackWhale = new Whale("Humbback Whale", "Antarctic", 919, 13, 13);
+
+		//Array
+		ArrayList<Whale> whales = new ArrayList<Whale>() {
+			{
+			add(rightWhale);
+			add(blueWhale);
+			add(spermWhale);
+			add(humpbackWhale);
+			}
+			}
+			;
+
+		//Print
+		System.out.println("Whale Details");
+		printAllWhalesInfo(whales);
+			
+		}
+		
+			
+		//First Print
+		public static void printAllWhalesInfo(ArrayList<Whale> whales) {
+			for (Whale whale : whales) {
+		System.out.println(whale.toString() + "\n");
+		}
+	}		
+}
+
